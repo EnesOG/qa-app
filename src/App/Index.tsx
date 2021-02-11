24ece6c92b94ee4557d9bb9474f1ee7e70f735cb
Enes Sagir <6980048-sagirdigital@users.noreply.gitlab.com>
2021-02-11 11:56:33 +0100

    added form and submit

--- src/App/Index.tsx
+++ src/App/Index.tsx
@@ -1,5 +1,8 @@
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "../Components/Button/Button";
+import Input from "../Components/Input/Input";
 import Question from "../Components/QA/Question";
 import Spacing from "../Components/Spacing/Spacing";
 import {
@@ -7,11 +10,19 @@ import {
 	toggleSort,
 	qaSelector,
 	deleteEntry,
+	QuestionAndAnswer,
+	addQuestion,
 } from "../Reducers/qaReducer";
+import { nanoid } from "nanoid";
 
 const App = () => {
 	const { data, sort } = useSelector(qaSelector);
 	const dispatch = useDispatch();
+	const { register, handleSubmit, errors, reset } = useForm();
+	const onSubmit: SubmitHandler<QuestionAndAnswer> = (data, e) => {
+		dispatch(addQuestion({ ...data, id: nanoid() }));
+		return reset();
+	};
 
 	return (
 		<div>
@@ -25,12 +36,11 @@ const App = () => {
 								? a.question.localeCompare(b.question)
 								: b.question.localeCompare(a.question)
 						)
-						.map(({ question, answer }, i) => (
+						.map((q) => (
 							<Question
-								onDeleteClick={() => dispatch(deleteEntry(i))}
-								key={i}
-								question={question}
-								answer={answer}
+								onDeleteClick={() => dispatch(deleteEntry(q.id))}
+								key={q.id}
+								{...q}
 							/>
 						))
 				) : (
@@ -45,6 +55,37 @@ const App = () => {
 			<Button onClick={() => dispatch(deleteAll())} variant="danger">
 				Delete all questions
 			</Button>
+
+			<Spacing>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<h2>Create new question!</h2>
+					<Input
+						errors={errors}
+						register={register({
+							required: {
+								value: true,
+								message: "Please enter your question",
+							},
+						})}
+						name="question"
+						label="What is your question?"
+					/>
+					<Spacing>
+						<Input
+							errors={errors}
+							register={register({
+								required: {
+									value: true,
+									message: "Please enter your answer",
+								},
+							})}
+							name="answer"
+							label="What is your answer?"
+						/>
+					</Spacing>
+					<Button>Create question</Button>
+				</form>
+			</Spacing>
 		</div>
 	);
 };
