36a5eb9f350e28edaafff2756da2b267d277d3e3
Enes Sagir <6980048-sagirdigital@users.noreply.gitlab.com>
2021-02-11 11:09:05 +0100

    Updated Question component

--- src/Components/QA/Question.tsx
+++ src/Components/QA/Question.tsx
@@ -2,12 +2,25 @@ import { useState } from "react";
 import { QuestionAndAnswer } from "../../Reducers/qaReducer";
 import "./Question.scss";
 
-const Question = ({ question, answer }: QuestionAndAnswer) => {
+interface QuestionAndAnswerProps extends QuestionAndAnswer {
+	onDeleteClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+
+const Question = ({
+	question,
+	answer,
+	onDeleteClick,
+}: QuestionAndAnswerProps) => {
 	const [extended, setExtended] = useState(false);
 
 	return (
 		<div onClick={() => setExtended(!extended)} className="qa">
-			<div className="qa__question">{question}</div>
+			<div className="qa__header">
+				<div className="qa__question">{question}</div>
+				<div onClick={onDeleteClick} className="qa__delete">
+					Delete entry
+				</div>
+			</div>
 			<div className={`qa__answer ${extended && `qa__answer-active`}`}>
 				{answer}
 			</div>
